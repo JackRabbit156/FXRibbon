6c37fd3abc06fcbd0cc9f83d1b1f7121116aa45a
JackRabbit156 <git@frankrieger.com>
2024-11-21 19:26:35 +0100

    Include RibbonMenu in example

--- FXRibbon-samples/src/main/java/com/pixelduke/control/CompleteRibbonSample.java
+++ FXRibbon-samples/src/main/java/com/pixelduke/control/CompleteRibbonSample.java
@@ -27,22 +27,27 @@
 
 package com.pixelduke.control;
 
+import com.pixelduke.control.ribbon.RibbonTab;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import jfxtras.styles.jmetro8.JMetro;
-//import org.scenicview.ScenicView;
 
 import java.net.URL;
 
 public class CompleteRibbonSample extends Application {
+
     private static final String RESOURCE = "CompleteRibbonFXML.fxml";
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         URL resource = CompleteRibbonSample.class.getResource(RESOURCE);
         Parent root = FXMLLoader.load(resource);
         Scene scene = new Scene(root);
@@ -55,8 +60,4 @@ public class CompleteRibbonSample extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args)
-    {
-        launch(args);
-    }
 }
